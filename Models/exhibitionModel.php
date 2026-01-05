36b51d3ca0653a590ed07b574a793dfc8ba6229f
mosaddiktawsif <mosaddik.tawsif@gmail.com>
2026-01-06 01:34:49 +0600

    updated exhibitionModel.php

--- Models/exhibitionModel.php
+++ Models/exhibitionModel.php
@@ -12,7 +12,38 @@ function getAllExhibitions(){
     return $exhibitions;
 }
 
+function getExhibitionById($id){
+    $con = getConnection();
+    $sql = "select * from exhibitions where id='{$id}'";
+    $result = mysqli_query($con, $sql);
+    if(mysqli_num_rows($result) == 1){
+        return mysqli_fetch_assoc($result);
+    }else{
+        return false;
+    }
+}
 
+function addExhibition($exhibition){
+    $con = getConnection();
+    $sql = "insert into exhibitions values(null, '{$exhibition['title']}', '{$exhibition['type']}', '{$exhibition['deadline']}', '{$exhibition['created_by']}', 'active', NOW())";
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function deleteExhibition($id){
+    $con = getConnection();
+    $sql = "delete from exhibitions where id='{$id}'";
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+?>
 
 
 
