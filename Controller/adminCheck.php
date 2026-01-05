80d24c7cd196210bea415b91735eed32566f2ff4
mosaddiktawsif <mosaddik.tawsif@gmail.com>
2026-01-06 03:40:03 +0600

    adminCheck.php

---
+++ Controller/adminCheck.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+
+    if(!isset($_COOKIE['status'])){
+        header('location: login.php');
+        exit();
+    }
+
+    if($_SESSION['role'] != 'admin'){
+        header('location: home.php');
+        exit();
+    }
+?>
