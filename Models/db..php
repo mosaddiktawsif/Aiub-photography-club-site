10e4570da5a6973c5c3920933968743f95acacf2
mosaddiktawsif <mosaddik.tawsif@gmail.com>
2026-01-05 23:49:39 +0600

    db connection

---
+++ Models/db..php
@@ -0,0 +1,16 @@
+<?php
+
+    $host = "127.0.0.1";
+    $dbname = "exhibitions";
+    $dbuser = "root";
+    $dbpass = "";
+
+    function getConnection(){
+        global $host;
+        global $dbname;
+        global $dbpass;
+
+        return $con = mysqli_connect($host, $GLOBALS['dbuser'], $dbpass, $dbname);
+    }
+
+?>
