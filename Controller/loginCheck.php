410353535575c1caccb241c525c2d0212a87d98d
mosaddiktawsif <mosaddik.tawsif@gmail.com>
2026-01-06 04:00:43 +0600

    created loginCheck.php

---
+++ Controller/loginCheck.php
@@ -0,0 +1,35 @@
+<?php
+    require_once('../Models/userModel.php');
+    session_start();
+
+    if(isset($_POST['submit'])){
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if($username == "" || $password == ""){
+            echo "null username/password";
+        }else{
+
+            $user = ['username'=> $username, 'password'=>$password];
+            $status = login($user);
+            
+            if($status){
+                setcookie('status', 'true', time()+3000, '/');
+                $_SESSION['username'] = $status['username'];
+                $_SESSION['user_id'] = $status['id'];
+                $_SESSION['role'] = $status['role'];
+                
+                if($status['role'] == 'admin'){
+                    header('location: ../View/admin_home.php');
+                }else{
+                    header('location: ../View/home.php');
+                }
+            }else{
+                echo "invalid username/password";
+            }
+        }
+    }else{
+        header('location: ../View/login.php');
+    }
+?>
