1f0db462d88eb00d65d81634aeb3b2ab444e7cf9
mosaddiktawsif <mosaddik.tawsif@gmail.com>
2026-01-07 00:54:06 +0600

    updated signup Form

---
+++ Views/signup.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up - Exhibition System</title>
+</head>
+<body>
+ <form name ="signupForm" method="post" action="../Controllers/signupCheck.php" onsubmit = "return validateSigup()">
+<fieldset>
+        <legend>Sign Up</legend>
+        <table>
+            <tr>
+                <td>
+                    Username
+                </td>
+                <td><input type = 'text' name = "username" value = ""></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type = 'password' name = "password" value = ""></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type = 'email' name = "email" value = ""></td>
+            </tr>
+            <tr>
+                <td>Submit</td>
+                <td>
+                    <input type = 'submit' name = "subbmit" value = "Login">
+                    <a href="login.php">Login</a>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
+</body>
+</html>
