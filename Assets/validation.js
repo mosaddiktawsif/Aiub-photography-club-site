64ac73efbc6e3dc88fc879b12e5c60f2567f1d72
mosaddiktawsif <mosaddik.tawsif@gmail.com>
2026-01-07 12:38:52 +0600

    updated js

--- Assets/validation.js
+++ Assets/validation.js
@@ -1,3 +1,24 @@
+
+function validateLogin() {
+    var username = document.forms["loginForm"]["username"].value;
+    var password = document.forms["loginForm"]["password"].value;
+
+    if (username == "") 
+        {
+        alert("wrong username ");
+        return false;
+    }
+
+    if (password == "") 
+        {
+        alert(" worng password");
+        return false;
+    }
+
+    return true;
+}
+
+
 function validateSignUp()
 {
     var username = document.forms["signupForm"]["username"].value;
@@ -15,4 +36,62 @@ function validateSignUp()
         return false ;
     }
         return true;
+
+
+        
+}
+
+function validateExhibition()
+{
+    var title = document.forms["exhibitionForm"]["title"].value;
+    var type = document.forms["exhibitionForm"]["type"].value;
+    var deadline = document.forms["exhibitionForm"]["deadline"].value;
+
+         if (title == "")
+    {
+        alert ("Please fill the title  ");
+        return false; 
+    }
+            if (type == "")
+    {
+        alert ("Please fill the type ");
+        return false ;
+    }
+     if (deadline == "")
+    {
+        alert ("Deadline must be filled");
+        return false; 
+    }
+        return true;
+
+
+        
+}
+
+
+function validateExhibition()
+{
+    var title = document.forms["exhibitionForm"]["title"].value;
+    var type = document.forms["exhibitionForm"]["type"].value;
+    var deadline = document.forms["exhibitionForm"]["deadline"].value;
+
+         if (title == "")
+    {
+        alert ("Please fill the title  ");
+        return false; 
+    }
+            if (type == "")
+    {
+        alert ("Please fill the type ");
+        return false ;
+    }
+     if (deadline == "")
+    {
+        alert ("Deadline must be filled");
+        return false; 
+    }
+        return true;
+
+
+        
 }
