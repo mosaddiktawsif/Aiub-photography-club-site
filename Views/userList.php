16f4bb40b95b859be0d444213c29e43a7fc07730
mosaddiktawsif <mosaddik.tawsif@gmail.com>
2026-01-08 23:00:24 +0600

    branch 3

---
+++ Views/userList.php
@@ -0,0 +1,26 @@
+<?php 
+require_once('../Controllers/sessionCheck.php'); 
+require_once('../Models/userModel.php');
+$users = getAllUsers();
+?>
+<html>
+<head><link rel="stylesheet" href="../Assets/css/style.css"></head>
+<body>
+    <div class="container">
+        <h2>4.3 User Management</h2>
+        <table>
+            <tr><th>User</th><th>Status</th><th>Action</th></tr>
+            <?php foreach($users as $u){ ?>
+            <tr>
+                <td><?=$u['username']?></td>
+                <td><?=$u['status']?></td>
+                <td>
+                   <a href="../Controllers/userController.php?id=<?=$u['id']?>&action=ban">Ban</a> |
+                   <a href="../Controllers/userController.php?id=<?=$u['id']?>&action=unban">Unban</a>
+                </td>
+            </tr>
+            <?php } ?>
+        </table>
+    </div>
+</body>
+</html>
